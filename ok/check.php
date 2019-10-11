6b63666f8abc856638aac6d961db5794e7fa303d
Ernest PIETRYKA <pietryka@iut.univ-paris5.fr>
2019-10-11 16:42:44 +0200

    commit de la fin <3

---
+++ ok/check.php
@@ -0,0 +1,25 @@
+<?php 
+            $username = $_REQUEST['username'];
+            $password = $_REQUEST['password'];
+            $iq = 0;
+            $host ="localhost";
+            $db = 'iq';
+            $user = 'iq';
+            $pass = 'iq';
+            $charset = 'utf8mb4';
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $options = [
+                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                 PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            try {
+                $pdo = new PDO($dsn, $user, $pass, $options);
+            } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+            }
+            $stmt = $pdo->prepare("SELECT UserName FROM UserLogin where UserName =  $username");
+            $stmt->execute();
+            $stmt = $pdp->prepare("SELECT UserName FROM UserLogin where UserName =  $username");
+            $stmt->execute();
+            ?>
